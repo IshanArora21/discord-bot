f1be216a8055798157368f6fd257e48aa67a3da1
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2019-05-16 16:53:19 -0400

    call on ready event to console log & user activity

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const configuration = require(./config.json);
+const configuration = require("./config.json");
+
+client.on("ready", () => {
+  
+  console.log('Successfully started. Users: ${bot.users.size} Servers: ${bot.guilds.size}');
+  bot.user.setActivity('My prefix is !');
+  
+});
