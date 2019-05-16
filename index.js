d3bce54484af76e9c9fd77d4788bf15b82073930
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2019-05-16 16:53:56 -0400

    accidentally called client instead of bot

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 const configuration = require("./config.json");
 
-client.on("ready", () => {
+bot.on("ready", () => {
   
   console.log('Successfully started. Users: ${bot.users.size} Servers: ${bot.guilds.size}');
   bot.user.setActivity('My prefix is !');
