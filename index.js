c5023992a23f01e03c89099a6837468048de4d26
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2019-05-16 20:29:59 -0400

    ping and stop commands !1!1!1!1!1

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const configuration = require("./config.json");
-bot.login(configuration.token);
+const config = require("./config.json");
+bot.login(config.token);
 
 bot.on("ready", () => {
   
@@ -16,4 +15,26 @@ bot.on("guildCreate", guild => {
 
   console.log(`Joined a new server! (${guild.name}) This server has ${guild.memberCount} members currently!`);
   
+});
+
+bot.on("message", async message => {
+
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+
+    if(command === "ping") {
+ 
+      const m = await message.channel.send("Ping?");
+    }
+
+
+
+    if(command === "stop"){
+      bot.destroy();
+    }
 });
