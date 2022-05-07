c097995a672515755fc81891c59f5bf0b7d1e2b5
IshanArora21 <34289247+IshanArora21@users.noreply.github.com>
2022-05-07 15:20:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,22 +21,8 @@ client.once("disconnect", () => {
 
 client.on("message", async (message) => {
   if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  const serverQueue = queue.get(message.guild.id);
-
-  if (message.content.startsWith(`${prefix}play`)) {
-    execute(message, serverQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}skip`)) {
-    skip(message, serverQueue);
-    return;
-  } else if (message.content.startsWith(`${prefix}stop`)) {
-    stop(message, serverQueue);
-    return;
-  } else {
-    message.channel.send("You need to enter a valid command!");
-  }
+  if (message.author.id == 388052737889402902)
+    return message.channel.send("YOU HAVE NO BITCHES!");
 });
 
 async function execute(message, serverQueue) {
